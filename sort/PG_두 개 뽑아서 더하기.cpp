6064702392646d9c237de8f51be0e6fe7359ce68
lena-for-world <54100538+lena-for-world@users.noreply.github.com>
2021-06-27 10:07:38 +0900

    Update PG_두 개 뽑아서 더하기.cpp

--- sort/PG_두 개 뽑아서 더하기.cpp
+++ sort/PG_두 개 뽑아서 더하기.cpp
@@ -11,3 +11,17 @@ vector<int> solution(vector<int> numbers) {
     sort(answer.begin(), answer.end());
     return answer;
 }
+
+
+// 다른 코드
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    set<int> st;
+    for(int i = 0;i<numbers.size();++i){
+        for(int j = i+1 ; j< numbers.size();++j){
+            st.insert(numbers[i] + numbers[j]); // set은 중복을 허용하지 않고, 자동 정렬된다.
+        }
+    }
+    answer.assign(st.begin(), st.end()); // answer에 st 할당 (+ assign은 기존 데이터 삭제)
+    return answer;
+}
