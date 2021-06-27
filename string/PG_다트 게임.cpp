fdf49c2d2cb0f91f3589f0768919d29199356240
lena-for-world <54100538+lena-for-world@users.noreply.github.com>
2021-06-27 20:27:15 +0900

    Update PG_다트 게임.cpp

--- string/PG_다트 게임.cpp
+++ string/PG_다트 게임.cpp
@@ -55,3 +55,32 @@ int solution(string dartResult) {
     for(int i = 0; i < sum.size(); i++) answer += sum[i];
     return answer;
 }
+
+
+
+
+// 간단한 코드
+
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string dartResult) {
+    vector<int> v;
+    for(int i = 0, s = 0; i < dartResult.size(); ++i) {
+        if(dartResult[i] >= '0' && dartResult[i] <= '9')
+            s = s * 10 + dartResult[i] - '0';
+        else if(dartResult[i] == 'S') v.push_back(s), s = 0;
+        else if(dartResult[i] == 'D') v.push_back(s * s), s = 0;
+        else if(dartResult[i] == 'T') v.push_back(s * s * s), s = 0;
+        else if(dartResult[i] == '*') {
+            if(v.size() > 1) v.back() *= 2, v[v.size() - 2] *= 2;
+            else v.back() *= 2;
+        } else v.back() = -v.back();
+
+    }
+    int ans = 0;
+    for(int i = 0; i < v.size(); ++i) ans += v[i];
+    return ans;
+}
