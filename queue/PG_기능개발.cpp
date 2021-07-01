996aa0360feac5c1143a25e83bc7c777fa54b4b1
lena-for-world <54100538+lena-for-world@users.noreply.github.com>
2021-07-01 13:20:53 +0900

    Update PG_기능개발.cpp

--- queue/PG_기능개발.cpp
+++ queue/PG_기능개발.cpp
@@ -31,3 +31,32 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     return answer;
 }
 
+
+// O(N)의 더 
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+
+    queue <int> q;
+    for (int i = 0; i < progresses.size(); ++i)
+        q.push(ceil((double)(100 - progresses[i]) / (double)speeds[i]));
+
+    int num = 1;
+    int cur = q.front();
+    q.pop();
+    while (!q.empty()) {
+        if (q.front() <= cur) {
+            q.pop();
+            ++num;
+        }
+        else {
+            answer.push_back(num);
+            num = 1;
+            cur = q.front();
+            q.pop();
+        }
+    }
+    answer.push_back(num);
+
+    return answer;
+}
