1bfd6ecf887c057f55923c63d5d26fd5ca664e0f
lena-for-world <54100538+lena-for-world@users.noreply.github.com>
2021-07-01 13:19:27 +0900

    Create PG_기능개발.cpp

---
+++ queue/PG_기능개발.cpp
@@ -0,0 +1,33 @@
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    queue<int> q;
+    for(int i = 0; i < progresses.size(); i++) {
+        q.push(i);
+    }
+    // 하나씩 더하는 방법 말고
+    // ceil((double)(100 - progresses[i]) / (double)speeds[i])
+    // 이렇게 몇 일 걸리는지 미리 계산해서 큐에 넣어주는 방법도 있었다
+    // 훨씬 간단하다!!
+    while(!q.empty()) {
+        int cnt = 1;
+        int f = q.front(); int j = q.front(); q.pop();
+        while(j < progresses.size()) {
+            progresses[j] += speeds[j];
+            if(j == progresses.size()-1 && progresses[f] >= 100) break;
+            if(j == progresses.size()-1) j = f;
+            else j++;
+        }
+        while(!q.empty()) {
+            int f = q.front();
+            if(progresses[f] >= 100) {
+                q.pop();
+                cnt++;
+            }
+            else break;
+        }
+        answer.push_back(cnt);
+    }
+    return answer;
+}
+
