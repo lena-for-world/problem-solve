10dc18c3cdafb3b211909f22c3f14742cbc6649c
lena-for-world <54100538+lena-for-world@users.noreply.github.com>
2021-07-13 15:52:42 +0900

    Update BOJ_염색체.cpp

--- string/BOJ_염색체.cpp
+++ string/BOJ_염색체.cpp
@@ -12,7 +12,7 @@ int main() {
 		cin >> st;
 		int len = st.size()-1;
 		if(st[0] >= 'A' && st[0] <= 'F' && st[len] >= 'A' && st[len] <= 'F') {
-      // A, F, C가 한 개 이상 나올 수 있지만, 한 개만 있어도 조건을 만족하기에 충분하다
+      			// A, F, C가 한 개 이상 나올 수 있지만, 한 개만 있어도 조건을 만족하기에 충분하다
 			st.erase(unique(st.begin(), st.end()), st.end());
 			if(st.size() == 5) {
 				if(st[1] == 'A' && st[2] == 'F' && st[3] == 'C') {
