f0479f8ee7dda3078efb22cdb80637e1d94a5738
lena-for-world <54100538+lena-for-world@users.noreply.github.com>
2021-08-18 23:14:30 +0900

    Create BOJ_도시 분할 계획.cpp

---
+++ Greedy/BOJ_도시 분할 계획.cpp
@@ -0,0 +1,46 @@
+// 최소 스패닝 트리(MST) - 크루스칼 알고리즘
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 100000
+using namespace std;
+
+int uf[MAX];
+vector<pair<int, pair<int, int>>> edge;
+
+int find(int n) {
+	if(uf[n] < 0) return n;
+	return uf[n] = find(uf[n]);
+}
+
+bool merge(int a, int b) {
+	a = find(a);
+	b = find(b);
+	if(a == b) return false;
+	uf[b] = a;
+	return true;
+}
+
+int main() {
+	int N, M;
+	int a, b, c;
+	int total = 0, cnt = 0;
+	cin >> N >> M;
+	for(int i = 0; i < M; i++) {
+		cin >> a >> b >> c;
+		edge.push_back({c, {a-1, b-1}});
+	}
+	sort(edge.begin(), edge.end());
+	for(int i = 0; i < N; i++) uf[i] = -1;
+	for(int i = 0; i < M; i++) {
+		if(cnt == N-2) break;
+		int e1 = edge[i].second.first; int e2 = edge[i].second.second;
+		if(merge(e1, e2)) {
+			total += edge[i].first;
+			cnt += 1;
+		}
+	}
+	cout << total;
+	return 0;
+}
