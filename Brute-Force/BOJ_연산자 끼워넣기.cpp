29f768f44c0916feef0de3b68177524ec3b026e9
lena-for-world <54100538+lena-for-world@users.noreply.github.com>
2021-09-13 20:25:31 +0900

    Update BOJ_연산자 끼워넣기.cpp

--- Brute-Force/BOJ_연산자 끼워넣기.cpp
+++ Brute-Force/BOJ_연산자 끼워넣기.cpp
@@ -60,3 +60,47 @@ int main() {
 	cout << mx << '\n' << mn;
 	return 0;
 }
+
+
+
+// 더 빠른 실행시간
+
+#include <iostream>
+#include <vector>
+#define MAX 1000000001
+using namespace std;
+
+int n;
+vector<int> num;
+int mx = -MAX, mn = MAX;
+
+void func(int plus, int minus, int multi, int div, int res, int depth){
+	if(depth == n-1) {
+		if(mx < res) mx = res;
+		if(mn > res) mn = res;
+		return;
+	}
+	if(plus != 0) func(plus-1, minus, multi, div, res+num[depth+1], depth+1);
+	if(minus != 0) func(plus, minus-1, multi, div, res-num[depth+1], depth+1);
+	if(multi != 0) func(plus, minus, multi-1, div, res*num[depth+1], depth+1);
+	if(div != 0) func(plus, minus, multi, div-1, res/num[depth+1], depth+1);
+}
+
+int main() {
+	int a;
+	int sign[4];
+	cin >> n;
+	for(int i = 0; i < n; i++) {
+		cin >> a;
+		num.push_back(a);
+	}
+	for(int i = 0; i < 4; i++) {
+		cin >> a;
+		sign[i] = a;
+	}
+	string st = "";
+	func(sign[0],sign[1],sign[2],sign[3], num[0], 0);
+	cout << mx << endl << mn;
+	return 0;
+}
+
